68e23e403e6b4b451db5326c0cd90f47d3234b4d
Evstratenko98 <evstratenko.sv98@gmail.com>
2021-08-24 12:58:02 +0400

    some changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,45 +1,53 @@
 const User = require("../models/User.js");
 
 class authController {
-   async login(req, res) {
-      try {
-         const { username, password } = req.body;
-         const user = await User.findOne({ username });
+    async login(req, res) {
+        try {
+            const { username, password, token } = req.body;
 
-         if (!user) return res.json({ message: "Пользователя не существует" });
-         if (user.password !== password)
-            return res.json({ message: "Пароли не совпадают" });
+            if (token) {
+                const user = await User.findOne({ _id: token });
+                if (user) return res.json(user);
+            }
 
-         res.cookie(`userId`, `${user._id}`, {
-            maxAge: 60 * 60 * 1000, //1 hour
-         });
-         res.json(user);
-      } catch (e) {
-         console.log(e);
-         res.status(200).json("Произошла ошибка");
-      }
-   }
+            const user = await User.findOne({ username });
+            if (!user)
+                return res
+                    .status(400)
+                    .json({ message: "Пользователя не существует" });
+            if (user.password !== password)
+                return res.status(400).json({ message: "Пароли не совпадают" });
 
-   async registration(req, res) {
-      try {
-         const { username, password, age, gender } = req.body;
-         const user = new User({
-            username,
-            password,
-            age,
-            gender,
-         });
-         await user.save();
+            res.cookie(`userId`, `${user._id}`, {
+                maxAge: 60 * 60 * 1000, //1 hour
+            });
+            res.json(user);
+        } catch (e) {
+            console.log(e);
+            res.status(200).json("Произошла ошибка");
+        }
+    }
 
-         res.cookie(`userId`, `${user._id}`, {
-            maxAge: 60 * 60 * 1000, //1 hour
-         });
-         res.json(user);
-      } catch (e) {
-         console.log(e);
-         res.status(200).json("Произошла ошибка");
-      }
-   }
+    async registration(req, res) {
+        try {
+            const { username, password, age, gender } = req.body;
+            const user = new User({
+                username,
+                password,
+                age,
+                gender,
+            });
+            await user.save();
+
+            res.cookie(`userId`, `${user._id}`, {
+                maxAge: 60 * 60 * 1000, //1 hour
+            });
+            res.json(user);
+        } catch (e) {
+            console.log(e);
+            res.status(200).json("Произошла ошибка");
+        }
+    }
 }
 
 module.exports = new authController();
